ff9edd61a15d9ced3a0d275d96bd075e23c86a9b
Jonathan Gehard Kohler <jonathan@local>
2019-07-09 11:19:30 -0300

    add feature: dados extras dos usuarios cadastrados no banco de dados

---
+++ src/services/apiDb.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import firebase from 'react-native-firebase'
+
+export default class apiDb extends React.Component {
+
+    constructor(collection) {
+        super();
+        this.ref = firebase.firestore().collection(collection);
+        //TODO adicionar tratamento caso a collection não exista
+        //TODO como adicionar credenciais para "escrever" na base?
+    }
+
+    add(data) {
+        this.ref.doc(data.id).set (data).
+            // add(data).
+            catch(error => this.setState({
+                errorMessage: error
+            }));
+            
+    }
+
+
+
+
+}
