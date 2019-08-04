08bec083dc6d7ee37f89c7ca86ee11f4f7e410fc
Diogo Bratti <dbratti@gmail.com>
2019-08-03 23:16:03 -0300

    Layout visualizar perfil

---
+++ src/data/MensagemTelefone.js
@@ -0,0 +1,2 @@
+const MensagemTelefone =  encodeURIComponent('Olá, tudo bem? Vi seu anúncio no aplicativo iCare. Você está disponível?');
+export default MensagemTelefone;
--- src/pages/anuncio/VisualizarAnuncio.js
+++ src/pages/anuncio/VisualizarAnuncio.js
@@ -1,19 +1,77 @@
-import React, { Component } from "react";
+import React from 'react';
 
-import { View, Text } from "react-native";
-
+import { View, Text, TouchableOpacity, Image, Linking, ScrollView } from "react-native";
+//import Ionicons from 'react-native-vector-icons/Ionicons';
+import { Icon } from 'react-native-elements';
+import MensagemTelefone from '../../data/MensagemTelefone';
 import { navigationOptions } from "../../styles/StyleBase";
-import StyleAnuncio from "../../styles/StyleAnuncio";
 
-export default class VisualizarAnuncio extends Component {
+import StyleAnuncio, {
+    anuncioIconeTelefone
+} from "../../styles/StyleAnuncio";
+
+export default class VisualizarAnuncio extends React.Component {
     static navigationOptions = navigationOptions;
 
+    constructor() {
+        super();
+    }
     render() {
         const anuncio = this.props.navigation.getParam('anuncio');
         return (
-            <View style={StyleAnuncio.container}>
-                <Text>{anuncio.nome}</Text>
+            <View style={StyleAnuncio.visualizarAnuncioContainer}>
+                <ScrollView>
+                    <View style={StyleAnuncio.visualizarAnuncioFotoContainer}>
+                        <Image
+                            style={StyleAnuncio.visualizarAnuncioImagemUsuario}
+                            source={{ uri: anuncio.foto }}
+                        />
+                    </View>
+                    <View style={StyleAnuncio.visualizarAnuncioTextosContainer}>
+                        <TouchableOpacity
+                            //style={styles.productButton} 
+                            onPress={() => {
+                                Linking.openURL('whatsapp://send?text=' + MensagemTelefone + '&phone=+55' + anuncio.telefone);
+                            }}
+                        >
+                            <View style={StyleAnuncio.visualizarAnuncioLinha}>
+                                <Text style={StyleAnuncio.visualizarAnuncioDescricaoText}>Nome: </Text>
+                                <Text style={StyleAnuncio.visualizarAnuncioAtributoText}>{anuncio.nome}</Text>
+                            </View>
+
+                            <View style={StyleAnuncio.visualizarAnuncioLinha}>
+                                <Text style={StyleAnuncio.visualizarAnuncioDescricaoText}>Telefone: </Text>
+                                <Icon
+                                    //raised //circulo em volta
+                                    name='whatsapp'
+                                    type='font-awesome'
+                                    //color={anuncioIconeTelefone.color}
+                                    size={anuncioIconeTelefone.size}
+                                    containerStyle={StyleAnuncio.iconeFiltro}
+                                //onPress={() => console.log('hello')} 
+                                />
+                                <Text style={StyleAnuncio.visualizarAnuncioAtributoText}>{anuncio.telefone}</Text>
+                            </View>
+                            <View style={StyleAnuncio.visualizarAnuncioLinha}>
+                                <Text style={StyleAnuncio.visualizarAnuncioDescricaoText}>Profissão: </Text>
+                                <Text style={StyleAnuncio.visualizarAnuncioAtributoText}>{anuncio.profissao}</Text>
+                            </View>
+
+                            <View style={StyleAnuncio.visualizarAnuncioLinha}>
+                                <Text style={StyleAnuncio.visualizarAnuncioDescricaoText}>Anúncio: </Text>
+                                <Text style={StyleAnuncio.visualizarAnuncioAtributoText}>{anuncio.anuncio}</Text>
+                            </View>
+                            <View style={StyleAnuncio.visualizarAnuncioLinha}>
+                                <Text style={StyleAnuncio.visualizarAnuncioDescricaoText}>Preço: </Text>
+                                <Text style={StyleAnuncio.visualizarAnuncioAtributoText}>R$ {anuncio.preco}*</Text>
+                            </View>
+                            <View style={StyleAnuncio.visualizarAnuncioLinha}>
+                                <Text style={StyleAnuncio.visualizarAnuncioDescricaoText}>* Estimado para o turno de 12 horas</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                </ScrollView>
             </View>
-        )
+        );
     }
 }
