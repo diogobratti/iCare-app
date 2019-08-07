9eeef07aee1c1c528823f7a39c4ed84176370fe7
Jonathan Gehard Kohler <jonathan@local>
2019-08-07 10:13:18 -0300

    atualizacao RN0.60.4

--- android/app/src/main/java/com/app/MainActivity.java
+++ android/app/src/main/java/com/app/MainActivity.java
@@ -9,15 +9,15 @@ public class MainActivity extends ReactActivity {
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
-    @Override
+    // @Override
     protected String getMainComponentName() {
         return "app";
     }
 
     // FBSdk
-     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
-    }
+    //  @Override
+    // public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    //     super.onActivityResult(requestCode, resultCode, data);
+    //     MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
+    // }
 }
--- android/app/src/main/java/com/app/MainApplication.java
+++ android/app/src/main/java/com/app/MainApplication.java
@@ -1,34 +1,35 @@
  package com.app;
 
 import android.app.Application;
+import android.util.Log;
 
+import com.facebook.react.PackageList;
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
-import co.apptailor.googlesignin.RNGoogleSigninPackage;
-import io.invertase.firebase.RNFirebasePackage;
+// import com.facebook.CallbackManager;
+// import com.facebook.FacebookSdk;
+// import com.facebook.appevents.AppEventsLogger;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
+//import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 
-import java.util.Arrays;
+
+//import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-// FBSdk
-  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
-// FBSdk
-  protected static CallbackManager getCallbackManager() {
-    return mCallbackManager;
-  }
+// // FBSdk
+//   private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+// // FBSdk
+//   protected static CallbackManager getCallbackManager() {
+//     return mCallbackManager;
+//   }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -38,7 +39,14 @@ public class MainApplication extends Application implements ReactApplication {
 
   @Override
   protected List<ReactPackage> getPackages() {
-    return Arrays.<ReactPackage>asList(
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new MyReactNativePackage());
+       packages.add(new RNFirebaseFirestorePackage());
+      return packages;
+    }
+/* return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
           new FBSDKPackage(mCallbackManager),
           new RNGoogleSigninPackage(),
@@ -49,7 +57,7 @@ public class MainApplication extends Application implements ReactApplication {
           new RNFirebaseAuthPackage(),
           new RNFirebaseFirestorePackage()
     );
-  }
+  }*/
 
     @Override
     protected String getJSMainModuleName() {
@@ -67,6 +75,6 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
     //FacebookSDK
-    AppEventsLogger.activateApp(this);
+    // AppEventsLogger.activateApp(this);
   }
 }
