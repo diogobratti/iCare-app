d1942ea120a1f3b600eebaf3e2d4d686a1d1c3fb
Jonathan Gehard Kohler <jonathan@local>
2019-12-09 23:38:53 -0300

    add Firebase dependencias e resolvido problemas build ios

--- ios/app/AppDelegate.m
+++ ios/app/AppDelegate.m
@@ -11,11 +11,17 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <Firebase.h>;
+
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"app"
                                             initialProperties:nil];
