9eac95050e0ce5213259d3d548ff6748f5f72a3d
Diogo Bratti <dbratti@gmail.com>
2019-12-09 23:38:53 -0300

    Dropdown municipio e estado

---
+++ src/styles/StyleLocalidade.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+import StyleBase, { navigationOptions, definicoesBase } from "./StyleBase";
+
+const StyleAnuncio = StyleSheet.create({
+    //entre cabecalho e rodape
+    container: {
+        flexDirection: 'column',
+        backgroundColor: definicoesBase.backgroundGeral,
+    },
+});
