9d992b90d15dde8a3ea3c748d6f87c430fe74f40
Diogo Bratti <dbratti@gmail.com>
2019-12-09 23:38:53 -0300

    exemplo com abas

---
+++ src/config/StatusBarConfig.js
@@ -0,0 +1,6 @@
+import { StatusBar } from "react-native";
+
+//para android
+StatusBar.setBackgroundColor("#DA552F");
+//para iOS
+StatusBar.setBarStyle("light-content");
---
+++ src/routes.js
@@ -0,0 +1,152 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator, createAppContainer } from "react-navigation";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import Main from "./pages/main";
+import Product from "./pages/product";
+
+import ListagemAnuncio from "./pages/anuncio/ListagemAnuncio";
+import ListagemConversa from "./pages/chat/ListagemConversa";
+import PerfilAnuncio from "./pages/prestador/PerfilAnuncio";
+import Perfil from "./pages/cliente/Perfil";
+
+
+class IconWithBadge extends React.Component {
+  render() {
+    const { name, badgeCount, color, size } = this.props;
+    return (
+      <View style={{ width: 24, height: 24, margin: 5 }}>
+        <Ionicons name={name} size={size} color={color} />
+        {badgeCount > 0 && (
+          <View
+            style={{
+              // /If you're using react-native < 0.57 overflow outside of the parent
+              // will not work on Android, see https://git.io/fhLJ8
+              position: 'absolute',
+              right: -6,
+              top: -3,
+              backgroundColor: 'red',
+              borderRadius: 6,
+              width: 12,
+              height: 12,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>
+              {badgeCount}
+            </Text>
+          </View>
+        )}
+      </View>
+    );
+  }
+}
+
+const HomeIconWithBadge = props => {
+  // You should pass down the badgeCount in some other ways like context, redux, mobx or event emitters.
+  return <IconWithBadge {...props} badgeCount={3} />;
+};
+
+const getTabBarIcon = (navigation, focused, tintColor) => {
+  const { routeName } = navigation.state;
+  let IconComponent = Ionicons;
+  let iconName;
+  if (routeName === 'Home') {
+    iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+    // We want to add badges to home tab icon
+    IconComponent = HomeIconWithBadge;
+  } else if (routeName === 'Chat') {
+    iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+  } else if (routeName === 'Anúncio') {
+    iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+  } else if (routeName === 'Perfil') {
+    iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+  } else if (routeName === 'Teste') {
+    iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+  }
+
+  // You can return any component that you like here!
+  return <IconComponent name={iconName} size={25} color={tintColor} />;
+};
+
+
+const stack = createStackNavigator({
+    Main,
+    Product,
+},{
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor: "#DA552F"
+        },
+        headerTintColor: "#FFF"
+    },
+});
+
+const MenuHome = createStackNavigator({
+    ListagemAnuncio,
+},{
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor: "#DA552F"
+        },
+        headerTintColor: "#FFF"
+    },
+});
+
+const MenuChat = createStackNavigator({
+    ListagemConversa,
+},{
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor: "#DA552F"
+        },
+        headerTintColor: "#FFF"
+    },
+});
+
+const MenuAnuncio = createStackNavigator({
+    PerfilAnuncio,
+},{
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor: "#DA552F"
+        },
+        headerTintColor: "#FFF"
+    },
+});
+
+const MenuPerfil = createStackNavigator({
+    Perfil,
+},{
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor: "#DA552F"
+        },
+        headerTintColor: "#FFF"
+    },
+});
+
+const MenuPrincipal = createBottomTabNavigator(
+    {
+        Home: { screen: MenuHome },
+        Chat: { screen: MenuChat },
+        Anúncio: { screen: MenuAnuncio },
+        Perfil: { screen: MenuPerfil },
+        Teste: { screen: stack }
+    },
+    {
+      defaultNavigationOptions: ({ navigation }) => ({
+        tabBarIcon: ({ focused, tintColor }) =>
+          getTabBarIcon(navigation, focused, tintColor),
+      }),
+      tabBarOptions: {
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      },
+    }
+);
+
+const Routes = createAppContainer(MenuPrincipal);
+
+export default Routes;
