f687edc6ae9c51cf89b7b9f6ce4222fce026b954
Jonathan Gehard Kohler <jonathangk.icare@gmail.com>
2019-12-09 23:38:53 -0300

    update RN0.61

--- android/app/src/main/java/br/com/icare/familia/MainApplication.java
+++ android/app/src/main/java/br/com/icare/familia/MainApplication.java
@@ -1,11 +1,12 @@
- package br.com.icare.familia;
+package br.com.icare.familia;
 
 import android.app.Application;
-import android.util.Log;
+//import android.util.Log;
 
+import android.content.Context;
 import com.facebook.react.PackageList;
-import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
-import com.facebook.react.bridge.JavaScriptExecutorFactory;
+//import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
+//import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
 // import com.facebook.CallbackManager;
 // import com.facebook.FacebookSdk;
@@ -16,6 +17,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 //import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import java.lang.reflect.InvocationTargetException;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 
 import io.invertase.firebase.admob.RNFirebaseAdMobPackage;
@@ -35,11 +37,12 @@ public class MainApplication extends Application implements ReactApplication {
 //     return mCallbackManager;
 //   }
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-  }
+  private final ReactNativeHost mReactNativeHost = 
+      new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+          return BuildConfig.DEBUG;
+        }
 
   @Override
   protected List<ReactPackage> getPackages() {
@@ -83,6 +86,7 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+    initializeFlipper(this); // Remove this line if you don't want Flipper enabled
     //FacebookSDK
     // AppEventsLogger.activateApp(this);
     // FacebookSdk.sdkInitialize(getApplicationContext());
@@ -91,5 +95,31 @@ public class MainApplication extends Application implements ReactApplication {
     //https://dev-yakuza.github.io/en/react-native/react-native-firebase-admob/
     //set your App ID created in Google Admob to MobileAds.initialize(this, "ad app id");. if you don’t know how to create App ID in Google Admob, see Google Admob blog.
     MobileAds.initialize(this, "????");
+  }
+
+  /**
+   * Loads Flipper in React Native templates.
+   *
+   * @param context
+   */
+  private static void initializeFlipper(Context context) {
+    if (BuildConfig.DEBUG) {
+      try {
+        /*
+         We use reflection here to pick up the class that initializes Flipper,
+        since Flipper library is not available in release mode
+        */
+        Class<?> aClass = Class.forName("com.facebook.flipper.ReactNativeFlipper");
+        aClass.getMethod("initializeFlipper", Context.class).invoke(null, context);
+      } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+      } catch (NoSuchMethodException e) {
+        e.printStackTrace();
+      } catch (IllegalAccessException e) {
+        e.printStackTrace();
+      } catch (InvocationTargetException e) {
+        e.printStackTrace();
+      }
+    }
   }
 }
