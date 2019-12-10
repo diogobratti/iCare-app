cd5c1354f2c01e8b133c28626421066b35024a78
Jonathan Gehard Kohler <jonathan@local>
2019-12-09 23:38:53 -0300

    versao inicial tela cadastro

--- src/pages/auth/SignUp.js
+++ src/pages/auth/SignUp.js
@@ -1,10 +1,24 @@
 // SignUp.js
 import React from 'react'
 import { StyleSheet, Text, TextInput, View, Button } from 'react-native'
+import { CheckBox, ThemeProvider } from 'react-native-elements'
+
 import firebase from 'react-native-firebase'
 
 export default class SignUp extends React.Component {
-    state = { email: '', password: '', errorMessage: null }
+    state = {
+        email: '',
+        password: '',
+        errorMessage: null,
+        cpf: '',
+        name: '',
+        birthdate: '',
+        phoneNumber: '',
+        address: '',
+        serviceTermsVersion: '',
+        serviceTermsCheckbox: false,
+    }
+
     handleSignUp = () => {
         console.log('handleSignUp')
 
@@ -28,7 +42,7 @@ export default class SignUp extends React.Component {
     translateSignUpErrors(error) {
 
         switch (error.code) {
-            case 'auth/email-already-in-use	':
+            case 'auth/email-already-in-use':
                 message = "Endereço de email já cadastrado";
                 break;
             case 'auth/invalid-email':
@@ -52,11 +66,14 @@ export default class SignUp extends React.Component {
         return (
             <View style={styles.container}>
 
-                <Text>Cadastre-se</Text>
-                {this.state.errorMessage &&
-                    <Text style={{ color: 'red' }}>
-                        {this.state.errorMessage}
-                    </Text>}
+                <TextInput
+                    placeholder="CPF"
+                    keyboardType='numeric'
+                    style={styles.textInput}
+                    onChangeText={(cpf) => this.setState({ cpf })}
+                    value={this.state.cpf}
+                    maxLength={11}
+                />
 
                 <TextInput
                     placeholder="Email"
@@ -67,20 +84,77 @@ export default class SignUp extends React.Component {
                 />
 
                 <TextInput
+                    placeholder="Nome"
+                    autoCapitalize="none"
+                    style={styles.textInput}
+                    onChangeText={name => this.setState({ name })}
+                    value={this.state.name}
+                />
+
+                <TextInput
+                    placeholder="Data de Nascimento"
+                    autoCapitalize="none"
+                    style={styles.textInput}
+                    onChangeText={birthdate => this.setState({ birthdate })}
+                    value={this.state.birthdate}
+                />
+
+                <TextInput
+                    placeholder="Telefone"
+                    autoCapitalize="none"
+                    style={styles.textInput}
+                    onChangeText={phoneNumber => this.setState({ phoneNumber })}
+                    value={this.state.phoneNumber}
+                />
+
+                <TextInput
+                    placeholder="Endereço"
+                    autoCapitalize="none"
+                    style={styles.textInput}
+                    onChangeText={address => this.setState({ address })}
+                    value={this.state.address}
+                />
+
+
+                <TextInput
                     secureTextEntry
-                    placeholder="Password"
+                    placeholder="Senha"
                     autoCapitalize="none"
                     style={styles.textInput}
                     onChangeText={password => this.setState({ password })}
                     value={this.state.password}
                 />
 
+                <TextInput
+                    secureTextEntry
+                    placeholder="Confirme a Senha"
+                    autoCapitalize="none"
+                    style={styles.textInput}
+                // onChangeText={password => this.setState({ password })}
+                // value={this.state.password}
+                />
+
+                <CheckBox
+                    title="Concordo com os Termos de Serviço"
+                    onValueChange={serviceTermsCheckbox => this.setState( {serviceTermsCheckbox})}
+                    // itemCheckedKey="RNchecked"
+                    // iconSize={16}
+                    value={this.state.serviceTermsCheckbox}
+                // checked={this.state.serviceTermsCheckbox}
+
+                />
+
                 <Button title="Cadastre-se" onPress={this.handleSignUp} />
+                {this.state.errorMessage &&
+                    <Text style={{ color: 'red' }}>
+                        {this.state.errorMessage}
+                    </Text>}
 
                 <Button
                     title="Já tem uma conta? Login"
                     onPress={() => this.props.navigation.navigate('Login')}
                 />
+
             </View>
         )
     }
