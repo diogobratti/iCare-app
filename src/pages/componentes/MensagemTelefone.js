0e4d6093297c59b26a4f2e732932e695fc56f6fe
Diogo Bratti <dbratti@gmail.com>
2019-12-09 23:38:53 -0300

    guarda qual anuncio foi para o whatsapp

--- src/pages/componentes/MensagemTelefone.js
+++ src/pages/componentes/MensagemTelefone.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 import { Linking } from "react-native";
+import firebase from 'react-native-firebase';
 
 const mensagem =  encodeURIComponent('Olá, tudo bem? Vi seu anúncio no aplicativo iCare. Você está disponível?');
 
-const MensagemTelefone = (telefone) => {
-  Linking.openURL('whatsapp://send?text=' + mensagem + '&phone=+55' + telefone);
-
+const MensagemTelefone = (data) => {
+  firebase.firestore().collection('transacoes').add({
+    UID: data.UID,
+    telefone: data.telefone,
+    timestamp: Date.now(),
+  });
+  Linking.openURL('whatsapp://send?text=' + mensagem + '&phone=+55' + data.telefone);
 };
 export default MensagemTelefone;
