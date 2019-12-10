c021a6ce7c9f689f45c74b0355770c1c5c475ce0
Jonathan Gehard Kohler <jonathangk.icare@gmail.com>
2019-12-09 23:38:53 -0300

    mais alteracoes googlesignin

--- src/pages/componentes/InputNome.js
+++ src/pages/componentes/InputNome.js
@@ -2,12 +2,16 @@ import React from 'react';
 import {Input, Icon} from 'react-native-elements';
 
 const InputNome = (props) => {
+  
+  const { onChangeText, children } = props;
+
   return (
     <Input
       label="Qual é o seu nome completo?"
       placeholder="ex: Fulano da Silva"
       leftIcon={<Icon name="user" type="antdesign" size={24} color="#007aff" />}
       errorMessage="Digite o nome completo"
+      onChangeText={onChangeText}
     />
   );
 };
